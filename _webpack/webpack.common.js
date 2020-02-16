b5d88154d7654a1590737d3fb453c9059b9220f1
Priya Porwal <priya.spcres@gmail.com>
2020-02-16 23:23:27 +0530

    firebase and circle ci addition

---
+++ _webpack/webpack.common.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/client/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, '../_dist'),
+    filename: 'app.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'My website',
+      template: path.resolve(__dirname, '../_templates/client.html'),
+    }),
+  ],
+};
