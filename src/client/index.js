8347f4216ebda1d57a73c1bb4763468e1e82d524
Priya Porwal <priya.spcres@gmail.com>
2020-02-16 23:59:33 +0530

    adding button

--- src/client/index.js
+++ src/client/index.js
@@ -1,10 +1,30 @@
+/* eslint-disable no-magic-numbers */
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
 class App extends Component {
+  state = {
+    count: 0,
+  };
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  };
+
   render() {
-    return <h1>Hello, World</h1>;
+    return <div>
+      <h1>{this.state.count}</h1>
+      <button onClick={this.increment}>  + </button>
+      <button onClick={this.decrement}> - </button>
+    </div>
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<div><App /> <App /></div>, document.getElementById('root'));
