211403e57a2a3dbe7bd8083c6598ee50826eaa35
Priya Porwal <priya.spcres@gmail.com>
2020-02-19 23:24:10 +0530

    web-dev-server and sourcemap addition

--- _webpack/webpack.dev.js
+++ _webpack/webpack.dev.js
@@ -1,6 +1,15 @@
 const common = require("./webpack.common");
 const merge = require("webpack-merge");
+const path = require('path');
 
 module.exports = merge(common, {
   mode: "development",
+
+  devServer: {
+    contentBase: path.join(__dirname, '../_dist'),
+    compress: true,
+    port: 9000,
+  },
+
+  devtool: 'inline-source-map',
 });
